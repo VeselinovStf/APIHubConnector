204317d6687e72a2e3804dd5716bfbc1a0b65d47
Stv <veselinoStf@gmail.com>
2020-02-11 09:34:22 +0200

    adding asp net core service configuration ext class

---
+++ APIHubConnector.Services/APIHubConnectorServiceConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using APIHubConnector.Services.Models;
+using APIHubConnector.Services.Netlify.DTOs;
+using APIHUbConnector.Core.Interfaces;
+using APIHUbConnector.Services.GitLab;
+using APIHUbConnector.Services.Netlify;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIHubConnector.Services
+{
+    public static class APIHubConnectorServiceConfiguration
+    {
+        public static void ConfigureAPIConnector(this IServiceCollection services)
+        {
+            services.AddScoped<IAPIRepoClientService<BaseResponse>, GitLabAPIClientService>();
+            services.AddScoped<IRepoUserKey<BaseResponse>, GitLabAPIClientService>();
+            services.AddScoped<IAPIHostClientService<BaseResponse>, NetlifyApiClientService>();
+            services.AddScoped<IHostDeployToken<DeplayKeyResponseDTO>, NetlifyApiClientService > ();
+
+        }
+    }
+}
